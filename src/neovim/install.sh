e99e32dda390ce07316331b6fdda3114b3879433
Jonatan Borkowski <jonatan@thebo.me>
2025-07-21 14:11:49 +0200

    Fix dirrr

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -25,7 +25,8 @@ ln -sf /usr/local/nvim/bin/nvim /usr/local/bin/nvim
 rm -rf /tmp/neovim-${VERSION}
 
 if [ "$INSTALLDOTFILES" = "true" ]; then
-  TEMP_DIR="$(mktemp -d)"
+  TEMP_DIR="/tmp/dotfiles"
+  mkdir -p $TEMP_DIR
   git clone "$DOTFILESREPOSITORY" "$TEMP_DIR"
 
   mkdir -p "$HOME/.config"
